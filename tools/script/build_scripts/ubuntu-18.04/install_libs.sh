bf1ee95fad64b567125ade3c4442b8d485771c62
Prashant K. Jha <pjha.sci@gmail.com>
2021-09-05 16:10:40 -0500

    improve ubuntu install scripts, print paths to file

--- tools/script/build_scripts/ubuntu-18.04/install_libs.sh
+++ tools/script/build_scripts/ubuntu-18.04/install_libs.sh
@@ -9,7 +9,7 @@ BUILDTHREADS="12"
 INSTALLATGLOBAL="0" # if 1, then libraries will be installed in /usr/local <-- for containers
 
 cmake_build="0" # set to 1 to install
-hpx_build="1"
+hpx_build="0"
 
 # some preliminary setup
 if [ ! -d "$SOURCEDIR" ]; then
@@ -19,10 +19,20 @@ if [ ! -d "$BUILDDIR" ]; then
     mkdir -p "$BUILDDIR"
 fi
 
-echo "SCRIPTPATH = $SCRIPTPATH"
-echo "SOURCEDIR = $SOURCEDIR"
-echo "BUILDDIR = $BUILDDIR"
-echo "Shell: $(which sh)"
+## open a file to write various key paths for subsequent use
+path_file="$SCRIPTPATH/paths.txt"
+
+# if path file exists, delete it so that it is created a fresh
+if [[ -f  $path_file ]]; then
+  echo "Path file exists, deleting it"
+  rm $path_file
+fi
+
+echolog()
+(
+  echo "$@"
+  echo "$@" >> $path_file
+)
 
 ## process input
 BUILD_TYPE="$1"
@@ -36,9 +46,18 @@ if [ "$BUILD_TYPE" != "Debug" ] && [ "$BUILD_TYPE" != "Release" ] && [ "$BUILD_T
     exit -1
 fi
 
-echo "<<<<<<<<<<< >>>>>>>>>>>"
-echo "CMAKE"
-echo "<<<<<<<<<<< >>>>>>>>>>>"
+echo " "
+echolog "SCRIPTPATH=\"$SCRIPTPATH\"
+SOURCEDIR=\"$SOURCEDIR\"
+BUILDDIR=\"$BUILDDIR\""
+
+echo "Shell: $(which sh)
+BUILD_TYPE=$BUILD_TYPE
+"
+
+echo "<<<<<<<<<<< >>>>>>>>>>>
+CMAKE
+<<<<<<<<<<< >>>>>>>>>>>"
 CMAKE_VER="3.19.4"
 CMAKE_TAR_FILE="cmake-$CMAKE_VER.tar.gz"
 CMAKE_INSTALL_PATH=$SCRIPTPATH/local/cmake/$CMAKE_VER/$BUILD_TYPE
@@ -48,8 +67,12 @@ fi
 CMAKE_BUILD_PATH=$BUILDDIR/cmake/$BUILD_TYPE/
 CMAKE_SOURCE_DIR=$SOURCEDIR/cmake/$CMAKE_VER
 CMAKE_EXE="$CMAKE_INSTALL_PATH/bin/cmake"
+
+# if we are not building cmake, then we most probably be using cmake
+# installed either at /usr/local/bin or /usr/bin (path of cmake is not important as long as it can be used simply by typing cmake in shell)
 if [[ $cmake_build -eq "0" ]]; then
   CMAKE_EXE="cmake"
+  CMAKE_INSTALL_PATH="/usr/bin"
 fi
 
 
@@ -71,253 +94,61 @@ if [[ $cmake_build -eq "1" ]]; then
   fi
 fi
 
-echo " "
-echo "Cmake exe is $CMAKE_EXE"
-echo " "
+echo " 
+CMAKE_INSTALL_PATH=$CMAKE_INSTALL_PATH 
+CMAKE_EXE=$CMAKE_EXE
+$($CMAKE_EXE --version)
+"
+echolog "
+## cmake
+CMAKE_INSTALL_PATH=\"$CMAKE_INSTALL_PATH\""
 
-echo "<<<<<<<<<<< >>>>>>>>>>>"
-echo "GMSH"
-echo "<<<<<<<<<<< >>>>>>>>>>>"
-# sudo apt install gmsh
-# brew install gmsh
+echo "<<<<<<<<<<< >>>>>>>>>>>
+GMSH
+<<<<<<<<<<< >>>>>>>>>>>
+ubuntu: sudo apt install gmsh
+mac: brew install gmsh
+"
 
-echo "<<<<<<<<<<< >>>>>>>>>>>"
-echo "BOOST"
-echo "<<<<<<<<<<< >>>>>>>>>>>"
-# sudo apt install libboost-all-dev
-# brew install boost
+echo "<<<<<<<<<<< >>>>>>>>>>>
+BOOST
+<<<<<<<<<<< >>>>>>>>>>>
+ubuntu: sudo apt install libboost-all-dev
+mac: brew install boost
+"
 
-# export BOOST_BUILD_TYPE=$(echo ${BUILD_TYPE/%WithDebInfo/ease} | tr '[:upper:]' '[:lower:]')
+echo "<<<<<<<<<<< >>>>>>>>>>>
+YAML
+<<<<<<<<<<< >>>>>>>>>>>
+ubuntu: sudo apt install libyaml-cpp-dev
+mac: brew install yaml-cpp
+"
 
-# BOOST_VER="1.68.0"
-# BOOST_TAR_FILE="boost_1_68_0.tar.bz2"
-# BOOST_INSTALL_PATH=$SCRIPTPATH/local/boost/$BOOST_VER/$BOOST_BUILD_TYPE
-# if [[ "$INSTALLATGLOBAL" -eq "1" ]]; then
-#   BOOST_INSTALL_PATH="/usr/local"
-# fi
-# BOOST_BUILD_PATH=$BUILDDIR/boost/$BUILD_TYPE/
-# BOOST_SOURCE_DIR=$SOURCEDIR/boost/$BOOST_VER
+echo "<<<<<<<<<<< >>>>>>>>>>>
+VTK
+<<<<<<<<<<< >>>>>>>>>>>
+ubuntu: sudo apt install libvtk7-dev
+mac: brew install vtk
+"
 
-# boost_build="0"
-# if [[ $boost_build -eq "1" ]]; then
-#   # download library
-#   cd $SOURCEDIR
-#   if [ ! -f "$BOOST_TAR_FILE" ]; then
-#     wget "http://downloads.sourceforge.net/project/boost/boost/${BOOST_VER}/$BOOST_TAR_FILE"
-#   fi
+echo "<<<<<<<<<<< >>>>>>>>>>>
+HWLOC
+<<<<<<<<<<< >>>>>>>>>>>
+ubuntu: sudo apt install libhwloc-dev
+mac: brew install hwloc
+"
 
-#   if [ ! -d $BOOST_SOURCE_DIR ]; then
-#     mkdir -p $BOOST_SOURCE_DIR
-#     tar xjf $BOOST_TAR_FILE -C $BOOST_SOURCE_DIR --strip-components=1
-#   fi
+echo "<<<<<<<<<<< >>>>>>>>>>>
+JEMALLOC
+<<<<<<<<<<< >>>>>>>>>>>
+ubuntu: sudo apt install libjemalloc-dev
+mac: brew install jemalloc
+"
 
-#   cd $BOOST_SOURCE_DIR
-#   ./bootstrap.sh --prefix=${BOOST_INSTALL_PATH} --with-toolset=gcc
-
-#   ./b2 -j${BUILDTHREADS} --with-atomic --with-iostreams --with-filesystem --with-program_options --with-regex --with-system --with-chrono --with-date_time --with-thread ${BOOST_BUILD_TYPE} install
-# fi
-
-echo "<<<<<<<<<<< >>>>>>>>>>>"
-echo "YAML"
-echo "<<<<<<<<<<< >>>>>>>>>>>"
-# sudo apt install libyaml-cpp-dev
-# brew install yaml-cpp
-
-# YAML_VER="0.6.3"
-# YAML_TAR_FILE="yaml-cpp-${YAML_VER}.tar.gz"
-# YAML_INSTALL_PATH=$SCRIPTPATH/local/yaml-cpp/$YAML_VER/$BUILD_TYPE
-# if [[ "$INSTALLATGLOBAL" -eq "1" ]]; then
-#   YAML_INSTALL_PATH="/usr/local"
-# fi
-# YAML_BUILD_PATH=$BUILDDIR/yaml-cpp/$BUILD_TYPE/
-# YAML_SOURCE_DIR=$SOURCEDIR/yaml-cpp/$YAML_VER
-
-# yaml_build="0"
-# if [[ $yaml_build -eq "1" ]]; then
-#   # download library
-#   cd $SOURCEDIR
-#   if [ ! -f "$YAML_TAR_FILE" ]; then
-#     wget "https://github.com/jbeder/yaml-cpp/archive/$YAML_TAR_FILE"
-#   fi
-
-#   if [ ! -d "$YAML_SOURCE_DIR" ]; then
-#     mkdir -p $YAML_SOURCE_DIR
-#     tar -zxf $YAML_TAR_FILE -C $YAML_SOURCE_DIR --strip-components=1
-#   fi
-
-#   # build library
-#   cd $BUILDDIR
-
-#   if [ ! -d "$YAML_BUILD_PATH" ]; then
-#     mkdir -p "$YAML_BUILD_PATH"
-#   else 
-#     rm -rf "$YAML_BUILD_PATH"
-#     mkdir -p "$YAML_BUILD_PATH"
-#   fi
-
-#   cd "$YAML_BUILD_PATH"
-
-#   $CMAKE_EXE -DCMAKE_BUILD_TYPE=$BUILD_TYPE   \
-#         -DCMAKE_INSTALL_PREFIX=$YAML_INSTALL_PATH \
-#         -DYAML_BUILD_SHARED_LIBS=ON \
-#         $YAML_SOURCE_DIR
-
-#   cd "$YAML_BUILD_PATH"
-#   make -j -l$BUILDTHREADS
-#   make install
-# fi
-
-echo "<<<<<<<<<<< >>>>>>>>>>>"
-echo "VTK"
-echo "<<<<<<<<<<< >>>>>>>>>>>"
-# sudo apt install libvtk7-dev
-# brew install vtk
-
-# VTK_VER_MAJOR="8.2"
-# VTK_VER="${VTK_VER_MAJOR}.0"
-# VTK_TAR_FILE="VTK-${VTK_VER}.tar.gz"
-# VTK_INSTALL_PATH=$SCRIPTPATH/local/vtk/$VTK_VER/$BUILD_TYPE
-# if [[ "$INSTALLATGLOBAL" -eq "1" ]]; then
-#   VTK_INSTALL_PATH="/usr/local"
-# fi
-# VTK_BUILD_PATH=$BUILDDIR/vtk/$BUILD_TYPE/
-# VTK_SOURCE_DIR=$SOURCEDIR/vtk/$VTK_VER
-
-# vtk_build="0"
-# if [[ $vtk_build -eq "1" ]]; then
-#   # download library
-#   cd $SOURCEDIR
-#   if [ ! -f "$VTK_TAR_FILE" ]; then
-#     wget "https://www.vtk.org/files/release/${VTK_VER_MAJOR}/${VTK_TAR_FILE}"
-#   fi
-
-#   if [ ! -d "$VTK_SOURCE_DIR" ]; then
-#     mkdir -p $VTK_SOURCE_DIR
-#     tar -zxf $VTK_TAR_FILE -C $VTK_SOURCE_DIR --strip-components=1
-#   fi
-
-#   # build library
-#   cd $BUILDDIR
-
-#   if [ ! -d "$VTK_BUILD_PATH" ]; then
-#     mkdir -p "$VTK_BUILD_PATH"
-#   else 
-#     rm -rf "$VTK_BUILD_PATH"
-#     mkdir -p "$VTK_BUILD_PATH"
-#   fi
-
-#   cd "$VTK_BUILD_PATH"
-
-#   $CMAKE_EXE -DCMAKE_BUILD_TYPE=$BUILD_TYPE  \
-#         -DCMAKE_INSTALL_PREFIX=$VTK_INSTALL_PATH \
-#         -DVTK_REQUIRE_LARGE_FILE_SUPPORT=ON \
-#         $VTK_SOURCE_DIR
-
-#         #-DCMAKE_INSTALL_RPATH=$VTK_INSTALL_PATH \
-#         # -DVTK_Group_Qt=ON \
-#         # -DVTK_QT_VERSION=5 \
-#         # -DVTK_Group_Imaging=ON \
-#         # -DVTK_Group_Views=ON \
-#         # -DModule_vtkRenderingFreeTypeFontConfig=ON \
-#         # -DVTK_WRAP_PYTHON=ON \
-#         # -DVTK_PYTHON_VERSION=3 \
-#         # -DPYTHON_EXECUTABLE=/usr/bin/python3 \
-#         # -DPYTHON_INCLUDE_DIR=/usr/include/python3.6 \
-#         # -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so \
-#         # -DBUILD_TESTING=OFF \
-#         # -DVTK_USE_SYSTEM_LIBRARIES=ON \
-#         # -DVTK_USE_SYSTEM_LIBPROJ4=OFF \
-#         # -DVTK_USE_SYSTEM_GL2PS=OFF \
-#         # -DVTK_USE_SYSTEM_LIBHARU=OFF \
-#         # -DVTK_USE_SYSTEM_PUGIXML=OFF \
-#         # $VTK_SOURCE_DIR
-
-#   cd "$VTK_BUILD_PATH"
-#   make -j -l$BUILDTHREADS
-#   make install
-# fi
-
-echo "<<<<<<<<<<< >>>>>>>>>>>"
-echo "HWLOC"
-echo "<<<<<<<<<<< >>>>>>>>>>>"
-# sudo apt install libhwloc-dev
-# brew install hwloc
-
-# HWLOC_VER="2.2.0"
-# HWLOC_TAR_FILE="hwloc-$HWLOC_VER.tar.gz"
-# HWLOC_INSTALL_PATH=$SCRIPTPATH/local/hwloc/$HWLOC_VER/$BUILD_TYPE
-# if [[ "$INSTALLATGLOBAL" -eq "1" ]]; then
-#   HWLOC_INSTALL_PATH="/usr/local"
-# fi
-# HWLOC_BUILD_PATH=$BUILDDIR/hwloc/$BUILD_TYPE/
-# HWLOC_SOURCE_DIR=$SOURCEDIR/hwloc/$HWLOC_VER
-
-# hwloc_build="0"
-# if [[ $hwloc_build -eq "1" ]]; then
-#   # download library
-#   cd $SOURCEDIR
-#   if [ ! -f "$HWLOC_TAR_FILE" ]; then
-#     wget "https://download.open-mpi.org/release/hwloc/v${HWLOC_VER%.*}/hwloc-${HWLOC_VER}.tar.gz"
-#   fi
-
-#   if [ ! -d "$HWLOC_SOURCE_DIR" ]; then
-#     mkdir -p $HWLOC_SOURCE_DIR
-#     tar -zxf $HWLOC_TAR_FILE -C $HWLOC_SOURCE_DIR --strip-components=1
-#   fi
-
-#   # build library
-#   cd $BUILDDIR
-
-#   if [ ! -d "$HWLOC_BUILD_PATH" ]; then
-#     mkdir -p "$HWLOC_BUILD_PATH"
-
-#     cd "$HWLOC_BUILD_PATH"
-
-#     $HWLOC_SOURCE_DIR/configure --prefix=${HWLOC_INSTALL_PATH} --disable-opencl 
-#   fi
-#   cd "$HWLOC_BUILD_PATH"
-#   make -j -l$BUILDTHREADS
-#   make install
-# fi
-
-echo "<<<<<<<<<<< >>>>>>>>>>>"
-echo "JEMALLOC"
-echo "<<<<<<<<<<< >>>>>>>>>>>"
-# sudo apt install libjemalloc-dev
-# brew install jemalloc
-
-# JEMALLOC_VER="5.1.0"
-# JEMALLOC_TAR_FILE="jemalloc-$JEMALLOC_VER.tar.bz2"
-# JEMALLOC_INSTALL_PATH=$SCRIPTPATH/local/jemalloc/$JEMALLOC_VER/$BUILD_TYPE
-# if [[ "$INSTALLATGLOBAL" -eq "1" ]]; then
-#   JEMALLOC_INSTALL_PATH="/usr/local"
-# fi
-# JEMALLOC_BUILD_PATH=$BUILDDIR/jemalloc/$BUILD_TYPE/
-# JEMALLOC_SOURCE_DIR=$SOURCEDIR/jemalloc/$JEMALLOC_VER
-
-# jemalloc_build="0"
-# if [[ $jemalloc_build -eq "1" ]]; then
-#   # download library
-#   cd $SOURCEDIR
-#   if [ ! -f "$JEMALLOC_TAR_FILE" ]; then
-#     wget "https://github.com/jemalloc/jemalloc/releases/download/${JEMALLOC_VER}/$JEMALLOC_TAR_FILE"
-#   fi
-
-#   if [ ! -d "$JEMALLOC_SOURCE_DIR" ]; then
-#     mkdir -p $JEMALLOC_SOURCE_DIR
-#     tar xjf $JEMALLOC_TAR_FILE -C $JEMALLOC_SOURCE_DIR --strip-components=1
-#   fi
-
-#   cd $JEMALLOC_SOURCE_DIR
-#   ./autogen.sh
-#   ./configure --prefix=${JEMALLOC_INSTALL_PATH}
-#   make -j -l$BUILDTHREADS
-#   make install
-# fi
-
-echo "<<<<<<<<<<< >>>>>>>>>>>"
-echo "HPX"
-echo "<<<<<<<<<<< >>>>>>>>>>>"
+echo "<<<<<<<<<<< >>>>>>>>>>>
+HPX
+<<<<<<<<<<< >>>>>>>>>>>
+"
 HPX_VER="1.3.0"
 HPX_TAR_FILE="${HPX_VER}.tar.gz"
 HPX_INSTALL_PATH=$SCRIPTPATH/local/hpx/$HPX_VER/$BUILD_TYPE
@@ -372,5 +203,26 @@ if [[ $hpx_build -eq "1" ]]; then
   make -j -l$BUILDTHREADS
   make install
 fi
+
+echolog "
+##
+HPX_INSTALL_PATH=\"$HPX_INSTALL_PATH\""
+
+## provide summary
+echo " 
+
+>> Dependencies are installed!!
+
+>> Here is summary of various paths (particularly note the HPX path that is needed to install PeriDEM)
+
+"
+
+echo "$(<$path_file)"
+
+echo " 
+
+>> Next, install PeriDEM using install_peridem.sh script
+
+"
 
 ) |& tee "build.log"
--- tools/script/build_scripts/ubuntu-18.04/install_peridem.sh
+++ tools/script/build_scripts/ubuntu-18.04/install_peridem.sh
@@ -8,6 +8,8 @@ BUILDDIR="$SCRIPTPATH/build/"
 BUILDTHREADS="12"
 INSTALLATGLOBAL="0" # if 1, then libraries will be installed in /usr/local <-- for containers
 
+peridem_build="0" # set to 1 to install and 0 to do nothing
+
 # some preliminary setup
 if [ ! -d "$SOURCEDIR" ]; then
     mkdir -p "$SOURCEDIR"
@@ -16,10 +18,20 @@ if [ ! -d "$BUILDDIR" ]; then
     mkdir -p "$BUILDDIR"
 fi
 
-echo "SCRIPTPATH = $SCRIPTPATH"
-echo "SOURCEDIR = $SOURCEDIR"
-echo "BUILDDIR = $BUILDDIR"
-echo "Shell: $(which sh)"
+## open a file to write various key paths for subsequent use
+path_file="$SCRIPTPATH/paths_peridem.txt"
+
+# if path file exists, delete it so that it is created a fresh
+if [[ -f  $path_file ]]; then
+  echo "Path file exists, deleting it"
+  rm $path_file
+fi
+
+echolog()
+(
+  echo "$@"
+  echo "$@" >> $path_file
+)
 
 ## process input
 BUILD_TYPE="$1"
@@ -33,67 +45,113 @@ if [ "$BUILD_TYPE" != "Debug" ] && [ "$BUILD_TYPE" != "Release" ] && [ "$BUILD_T
     exit -1
 fi
 
+echo " "
+echolog "SCRIPTPATH=\"$SCRIPTPATH\"
+SOURCEDIR=\"$SOURCEDIR\"
+BUILDDIR=\"$BUILDDIR\""
+
+echo "Shell: $(which sh)
+BUILD_TYPE=$BUILD_TYPE
+"
+
 # echo "<<<<<<<<<<< >>>>>>>>>>>"
 # echo "CMAKE"
 # echo "<<<<<<<<<<< >>>>>>>>>>>"
 CMAKE_EXE="cmake"
+CMAKE_INSTALL_PATH="/usr/bin"
 
 # echo "<<<<<<<<<<< >>>>>>>>>>>"
 # echo "HPX"
 # echo "<<<<<<<<<<< >>>>>>>>>>>"
-HPX_INSTALL_PATH="0"
+HPX_INSTALL_PATH="/home/prashant/work/peridem_works/test_PeriDEM_install_scripts/ubuntu-18.04/local/hpx/1.3.0/Release"
 if [[ $HPX_INSTALL_PATH -eq "0" ]]; then
   echo "Please provide path where HPX is installed in HPX_INSTALL_PATH variable and then rerun the script again."
+  exit
 fi
 
 
-echo "<<<<<<<<<<< >>>>>>>>>>>"
-echo "PERIDEM"
-echo "<<<<<<<<<<< >>>>>>>>>>>"
+echo "<<<<<<<<<<< >>>>>>>>>>>
+PERIDEM
+<<<<<<<<<<< >>>>>>>>>>>"
 PERIDEM_VER="0.1.0"
-PERIDEM_PATH=$SCRIPTPATH/peridem/$1/
+PERIDEM_BUILD_PATH=$SCRIPTPATH/peridem/$1/
 PERIDEM_SOURCE_DIR=$SOURCEDIR/peridem/$PERIDEM_VER
-PERIDEM_INSTALL_PATH=$SCRIPTPATH/local/peridem/$PERIDEM_VER/$BUILD_TYPE
 
-peridem_build="1"
 if [[ $peridem_build -eq "1" ]]; then
   # download library
   cd $SOURCEDIR
   
   if [ ! -d $PERIDEM_SOURCE_DIR ]; then
+    echo "Cloning PeriDEM"
     mkdir -p $PERIDEM_SOURCE_DIR
     git clone git@github.com:prashjha/PeriDEM.git $PERIDEM_SOURCE_DIR
   fi
 
   # build library
-  cd $PERIDEM_PATH
-
-  if [ ! -d "$PERIDEM_PATH" ]; then
-    mkdir -p "$PERIDEM_PATH"
+  if [ ! -d "$PERIDEM_BUILD_PATH" ]; then
+    echo "Creating PeriDEM build path"
+    mkdir -p "$PERIDEM_BUILD_PATH"
   else 
-    rm -rf $PERIDEM_PATH
-    mkdir -p $PERIDEM_PATH
+    echo "PeriDEM build path exist, deleting and then creating the dir again"
+    rm -rf $PERIDEM_BUILD_PATH
+    mkdir -p $PERIDEM_BUILD_PATH
   fi
 
-  cd "$PERIDEM_PATH"
+  cd "$PERIDEM_BUILD_PATH"
 
   # add when building in mac
   # -DYAML_CPP_DIR="/usr/local" \
   $CMAKE_EXE -DCMAKE_BUILD_TYPE=$BUILD_TYPE   \
-        -DCMAKE_INSTALL_PREFIX=$PERIDEM_INSTALL_PATH \
-        -DCMAKE_INSTALL_RPATH=$PERIDEM_INSTALL_PATH \
         -DHPX_DIR="$HPX_INSTALL_PATH/lib/cmake/HPX" \
         -DEnable_Documentation=ON \
         -DEnable_Tests=ON \
         $PERIDEM_SOURCE_DIR
 
-        #-DCMAKE_INSTALL_RPATH=$PERIDEM_INSTALL_PATH \
-        #-DYAML_CPP_DIR="$YAML_INSTALL_PATH" \
-        #-DVTK_DIR="$VTK_INSTALL_PATH/lib/cmake/vtk-${VTK_VER_MAJOR}" \
-
-  cd "$PERIDEM_PATH"
+  cd "$PERIDEM_BUILD_PATH"
+  echo "
+  Building library
+  "
   make -j -l$BUILDTHREADS
+  echo "
+  Running ctest
+  "
   ctest --verbose
 fi 
+
+if [[ -f ${PERIDEM_BUILD_PATH}/bin/PeriDEM ]]; then
+  echo "
+
+>> PeriDEM is built in dir 
+
+$PERIDEM_BUILD_PATH
+
+  "
+else
+  echo "
+
+>> PeriDEM executible not found! 
+
+>> Check if paths are specified correctly and HPX is installed
+
+>> Report issue if unable to fix the error!
+
+  "
+fi
+
+echo ">> Here is summary of key paths
+
+"
+
+echolog "
+## cmake
+CMAKE_INSTALL_PATH=\"$CMAKE_INSTALL_PATH\"
+
+## HPX
+HPX_INSTALL_PATH=\"$HPX_INSTALL_PATH\"
+
+## PeriDEM
+PERIDEM_SOURCE_DIR=\"$PERIDEM_SOURCE_DIR\"
+PERIDEM_BUILD_PATH=\"$PERIDEM_BUILD_PATH\"
+"
 
 ) |& tee "build_peridem.log"
