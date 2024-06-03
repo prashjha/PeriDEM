7e6cc5d5256adb3605229ea891df2a4dc1cdc227
Prashant K. Jha <pjha.sci@gmail.com>
2024-06-03 22:46:45 +0100

    update copyright heading in all files

--- src/fe/triElem.h
+++ src/fe/triElem.h
@@ -1,10 +1,11 @@
 /*
- * ----------------------------------
- * Copyright (c) 2021 Prashant K. Jha
- * ----------------------------------
+ * -------------------------------------------
+ * Copyright (c) 2021 - 2024 Prashant K. Jha
+ * -------------------------------------------
+ * PeriDEM https://github.com/prashjha/PeriDEM
  *
  * Distributed under the Boost Software License, Version 1.0. (See accompanying
- * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ * file LICENSE)
  */
 
 #ifndef FE_TRIELEM_H
