e8c7927cfc0056f1aadfeab2913274c0691e5c06
Prashant K. Jha <pjha.sci@gmail.com>
2024-10-04 05:17:59 +0000

    cleaning

--- dir_0c8d49197857a3656000369f17e7cb5c.js
+++
@@ -1,4 +0,0 @@
-var dir_0c8d49197857a3656000369f17e7cb5c =
-[
-    [ "inp", "dir_1c5aed2fc2d07791f7b0817d0ad75283.html", "dir_1c5aed2fc2d07791f7b0817d0ad75283" ]
-];
--- dir_f604082fb4472e7ee7974842b74168d9.js
+++
@@ -1,4 +0,0 @@
-var dir_f604082fb4472e7ee7974842b74168d9 =
-[
-    [ "problem_setup.py", "examples_2PeriDEM_2two__particles_2circ__no__damp_2inp_2problem__setup_8py.html", "examples_2PeriDEM_2two__particles_2circ__no__damp_2inp_2problem__setup_8py" ]
-];
