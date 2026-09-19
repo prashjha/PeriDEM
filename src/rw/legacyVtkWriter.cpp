6e712893e0c86af744d71b3516ee0d71615233fe
Prashant Kumar Jha <pjha.sci@gmail.com>
2026-09-18 21:09:22 -0600

    Align writer headers with the Boost license
    mshWriter and legacyVtkWriter still named GPLv3 and LICENSE.txt. The
    repository license is Boost 1.0.

--- src/rw/legacyVtkWriter.cpp
+++ src/rw/legacyVtkWriter.cpp
@@ -1,7 +1,12 @@
-// Copyright (c) 2021 Prashant K. Jha
-//
-// Distributed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
-// (See accompanying file LICENSE.txt)
+/*
+ * -------------------------------------------
+ * Copyright (c) 2021 - 2024 Prashant K. Jha
+ * -------------------------------------------
+ * PeriDEM https://github.com/prashjha/PeriDEM
+ *
+ * Distributed under the Boost Software License, Version 1.0. (See accompanying
+ * file LICENSE)
+ */
 
 #include "legacyVtkWriter.h"
 
